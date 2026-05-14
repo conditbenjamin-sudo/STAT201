b263f93a2c46f0a2ad42a32a63aefc10300ff310
Olya Wright <condit.benjamin@gmail.com>
2026-05-14 19:57:22 +0000

    Adding my R script

--- final project.R
+++ final project.R
@@ -1,8 +1,2 @@
 read.csv()
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote remove origin
-git remote add origin https://github.com/YOURUSERNAME/YOURREPO.git
-git push -u origin main
+ 
